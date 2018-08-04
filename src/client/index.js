01f49aa5123aaa9c811b354311cf653794688adf
dbmarch <dbmarch@gmail.com>
2018-08-03 22:18:15 -0500

    client reaches out to server and grabs a string

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { stringify } from "querystring";
 
-class Header extends React.Component {
-    render() {
+class App extends Component {
+  state = {
+    response: ""
+  };
 
-        return (
-            <div> Hello World.</div>
-        )
-    }
+  componentDidMount() {
+    console.log("componentDidMount");
+
+    fetch("/api/hello")
+      .then(res => {
+        console.log(res);
+        return res.json();
+      })
+      .catch(error => console.error("Error: ", error))
+      .then(response => {
+        console.log(response);
+        this.setState({ response });
+      });
+  }
+
+  render() {
+    const message = <div>Hello from React.</div>;
+
+    console.log(this.state.response);
+    const remoteMessage = <div> {this.state.response.express} </div>;
+
+    return (
+      <div>
+        {" "}
+        {message} {remoteMessage}
+      </div>
+    );
+    //return <div> Hi </div>;
+  }
 }
 
-ReactDOM.render(<Header/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/server/index.js
+++ src/server/index.js
@@ -3,14 +3,22 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 
 const app = express();
-const DEBUG = process.env.NODE_ENV !== 'production';
-const PORT = DEBUG ? '3000' : process.env.PORT;
+const DEBUG = process.env.NODE_ENV !== "production";
+const PORT = DEBUG ? "3000" : process.env.PORT;
 
-app.use(express.static(__dirname + '/../../public'));
-app.use(morgan('dev'));
+app.use(express.static(__dirname + "/../../public"));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(PORT, function () {
-    console.log('Express listening on port %s', PORT);
+app.get("/api/hello", (req, res) => {
+  console.log("/api/hello");
+  const message = { text: "Hello from Express" };
+  //res.json(message);
+  //res.send("Hello from Express");
+  res.status(200).send({ express: "Hello from Express!" });
+});
+
+const server = app.listen(PORT, function() {
+  console.log("Express listening on port %s", PORT);
 });
